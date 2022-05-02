db6438545f0166f2d420c9d78b0a0ddcdb29611b
Daniel Durham <dldurham.dd@gmail.com>
2022-05-01 23:47:28 -0400

    Passing arguments through url. Linking and inheriting templates, with minor bootstrap styling. Model registered with admin.

--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,9 @@ def all_projects(request):
     # query the database to return all project objects.
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html', {'projects': projects})
+
+
+def project_detail(request, pk):
+    # query db for a single porject using its primary key.
+    project = Project.objects.get(pk=pk)
+    return render(request, 'projects/project_detail.html', {'project': project})
